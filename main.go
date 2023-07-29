d3e9a86feab3712f4989c937a9a085bff2d1224f
Arpit Srivastava <arpit.srivastava@gojek.com>
2023-07-29 19:32:48 +0530

    logging

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"google.golang.org/grpc"
+	"log"
 	"os"
 	"source.golabs.io/scp/goto-profile-protos/gen/models"
 	"source.golabs.io/scp/goto-profile-protos/gen/services"
@@ -12,26 +13,31 @@ import (
 
 func main() {
 	fmt.Println("PINGING Singapore service from taiwan......")
-	_ = os.Setenv("GRPC_VERBOSITY", "DEBUG")
-	_ = os.Setenv("GRPC_TRACE", "all")
+	//_ = os.Setenv("GRPC_VERBOSITY", "DEBUG")
+	//_ = os.Setenv("GRPC_TRACE", "all")
+
+	// Set GRPC_GO_LOG_VERBOSITY environment variable to 'debug'
+	_ = os.Setenv("GRPC_GO_LOG_VERBOSITY", "debug")
+
+	// Set GRPC_GO_LOG_SEVERITY_LEVEL environment variable to 'debug'
+	_ = os.Setenv("GRPC_GO_LOG_SEVERITY_LEVEL", "debug")
 	conn, err := grpc.Dial("10.225.96.9:80", grpc.WithInsecure())
 	if err != nil {
-		fmt.Printf("[error] could not obtain gRPC connection. err: [%s]\n", err)
-		return
+		log.Fatalf("[error] could not obtain gRPC connection. err: [%s]", err)
 	}
 	defer conn.Close()
 
 	ctx := context.Background()
 	client := services.NewProfilePingServiceClient(conn)
 	if err != nil {
-		fmt.Printf("[error] could not get profileService client. err: [%s]\n", err)
+		log.Fatalf("[error] could not get profileService client. err: [%s]", err)
 		return
 	}
 	startTime := time.Now()
 	resp, err := client.Ping(ctx, &models.NoParam{})
 	if err != nil {
-		fmt.Printf("[error] from ping singapore PS. err: [%s]\n", err)
+		log.Fatalf("[error] from ping singapore PS. err: [%s]", err)
 		return
 	}
-	fmt.Printf("[response] timeTaken: [%v milliSeconds] from Singapore ProfileService: [%+v]", time.Since(startTime).Milliseconds(), resp)
+	log.Fatalf("[response] timeTaken: [%v milliSeconds] from Singapore ProfileService: [%+v]", time.Since(startTime).Milliseconds(), resp)
 }
